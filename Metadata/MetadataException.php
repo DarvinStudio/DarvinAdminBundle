a398f1c734758805d16653b6484954bdf1ac24d4
igor <igor.sv.n@gmail.com>
2015-08-19 09:33:20 +0300

    Change main PHPDoc blocks content.

--- Metadata/MetadataException.php
+++ Metadata/MetadataException.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: igor
- * Date: 04.08.15
- * Time: 10:03
- * To change this template use File | Settings | File Templates.
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Darvin\AdminBundle\Metadata;
--- View/ViewException.php
+++ View/ViewException.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: igor
- * Date: 07.08.15
- * Time: 10:47
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Darvin\AdminBundle\View;

88d7a14c2640c8ee7eddd6044fe84970a0f724c8
igor <igor.sv.n@gmail.com>
2015-08-20 11:35:01 +0300

    Mark forms as not submitted after AJAX complete.

--- Resources/public/scripts/globals.js
+++ Resources/public/scripts/globals.js
@@ -2,4 +2,6 @@ var AJAX_LOADER = '<span class="ajax_loader"></span>';
 
 $(document).bind('ajaxComplete', function () {
     $('.ajax_loader').remove();
+
+    $('form').removeData('submitted');
 });

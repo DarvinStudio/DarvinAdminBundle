9ac4e090a5337740f9a602bd3e2342dff45b7daf
igor <igor.sv.n@gmail.com>
2015-08-27 09:59:38 +0300

    Add removal functionality to Dropzone.

--- Resources/public/scripts/dropzone-init.js
+++ Resources/public/scripts/dropzone-init.js
@@ -7,21 +7,28 @@ $(document).ready(function () {
         var $files = $dropzone.parents('form').first().find($dropzone.data('files') + '[data-prototype]');
         var filePrototype = $files.data('prototype');
 
-        $dropzone.dropzone({
-            url:     $dropzone.data('url'),
-            success: function (file, response) {
+        var dropzone = new Dropzone('#' + $dropzone.attr('id'), {
+            acceptedFiles:      $dropzone.data('accepted-files'),
+            addRemoveLinks:     true,
+            dictDefaultMessage: Translator.trans('dropzone.default_message'),
+            dictFileTooBig:     Translator.trans('dropzone.file_too_big'),
+            dictRemoveFile:     Translator.trans('dropzone.remove_file'),
+            filesizeBase:       1024,
+            maxFilesize:        $dropzone.data('max-filesize'),
+            url:                $dropzone.data('url')
+        });
+
+        dropzone
+            .on('success', function (file, response) {
                 var $file = $(filePrototype.replace(/__name__/g, $files.children().length));
 
                 $file.find('.filename').val(response[0]);
                 $file.find('.original_filename').val(file.name);
 
                 $files.append($file);
-            },
-            acceptedFiles:      $dropzone.data('accepted-files'),
-            dictDefaultMessage: Translator.trans('dropzone.default_message'),
-            dictFileTooBig:     Translator.trans('dropzone.file_too_big'),
-            filesizeBase:       1024,
-            maxFilesize:        $dropzone.data('max-filesize')
-        });
+            })
+            .on('removedfile', function (file) {
+                $files.find('.original_filename[value="' + file.name + '"]').parents('.row:first').remove();
+            });
     });
 });

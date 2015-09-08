a26893a563a8fea63975947ac253a987973d31db
igor <igor.sv.n@gmail.com>
2015-09-08 10:09:44 +0300

    Implement feedback form CKEditor plugin.

---
+++ Resources/public/scripts/ckeditor/plugins/feedback_form/lang/en.js
@@ -0,0 +1,3 @@
+CKEDITOR.plugins.setLang('feedback_form', 'en', {
+    button: 'Feedback form'
+});
--- Resources/public/scripts/ckeditor/plugins/feedback_form/plugin.js
+++ Resources/public/scripts/ckeditor/plugins/feedback_form/plugin.js
@@ -1,17 +1,25 @@
 CKEDITOR.plugins.add('feedback_form', {
     requires: 'widget',
-    icons:    'feedback_form',
+    lang:     'en,ru',
     init:     function (editor) {
-        editor.addContentsCss(this.path + 'styles/feedback_form.css');
-
         editor.widgets.add('feedback_form', {
-            button:          'Add feedback form',
             template:        '<div class="feedback_form">%feedback_form%</div>',
             allowedContent:  'div(!feedback_form)',
             requiredContent: 'div(feedback_form)',
             upcast:          function (element) {
-                return 'div' === element.name && element.hasClass('feedback_form');
+                return 'div' === element.name
+                    && element.hasClass('feedback_form')
+                    && element.children.length > 0
+                    && '%feedback_form%' === element.children[0].value;
             }
         });
+
+        editor.ui.addButton('feedback_form', {
+            label:   editor.lang.feedback_form.button,
+            command: 'feedback_form',
+            icon:    this.path + 'images/icon.png?' + Math.random()
+        });
+
+        editor.addContentsCss(this.path + 'styles/styles.css?' + Math.random());
     }
 });

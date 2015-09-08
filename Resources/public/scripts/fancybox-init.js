ff9eeccaa0762e2d112a11cba470542738d12799
igor <igor.sv.n@gmail.com>
2015-09-08 11:08:15 +0300

    Add Fancybox translations.

--- Resources/public/scripts/fancybox-init.js
+++ Resources/public/scripts/fancybox-init.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+    $.fancybox.defaults.locales = $.extend({}, $.fancybox.defaults.locales, {
+        ru: {
+            CLOSE:      Translator.trans('fancybox.close'),
+            NEXT:       Translator.trans('fancybox.next'),
+            PREV:       Translator.trans('fancybox.prev'),
+            ERROR:      Translator.trans('fancybox.error'),
+            EXPAND:     Translator.trans('fancybox.expand'),
+            SHRINK:     Translator.trans('fancybox.shrink'),
+            PLAY_START: Translator.trans('fancybox.play_start'),
+            PLAY_STOP:  Translator.trans('fancybox.play_stop')
+        }
+    });
+
     $('body').on('click', 'a.fancybox_ajax', function (e) {
         e.preventDefault();
 

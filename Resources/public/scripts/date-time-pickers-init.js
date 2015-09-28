ba2c89e387ca846853f6bc7c6cc1e8e27c28a3b8
igor <igor.sv.n@gmail.com>
2015-09-28 15:44:39 +0300

    Use english locale as fallback for date-time-pickers.

--- Resources/public/scripts/date-time-pickers-init.js
+++ Resources/public/scripts/date-time-pickers-init.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     var locale = 'en' !== LOCALE ? LOCALE : '';
-    var options = $.extend({}, $.datepicker.regional[locale], $.timepicker.regional[locale], {
+    var translations = {
+        datepicker: $.datepicker.regional['undefined' !== typeof $.datepicker.regional[locale] ? locale : ''],
+        timepicker: $.timepicker.regional['undefined' !== typeof $.timepicker.regional[locale] ? locale : '']
+    };
+
+    var options = $.extend({}, translations.datepicker, translations.timepicker, {
         dateFormat: 'dd.mm.yy'
     });
 

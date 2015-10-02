18057dff584e7f8e73d837d5e0ae1890465e4e83
igor <igor.sv.n@gmail.com>
2015-10-02 15:00:21 +0300

    Update app JS.

--- Resources/scripts/app.js
+++ Resources/scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     /* показать/скрыть сайдбар */
-    $('#sidebar_switcher').on('mousedown', function () {
+    $('#sidebar_switcher').on('mousedown', function(){
         var switch_btn = $('#sidebar_switcher');
         var sidebar_wrap = $('.sidebar_wrap');
         var content_wrap = $('.content_wrap');
@@ -9,7 +9,7 @@ $(document).ready(function () {
         content_wrap.toggleClass('active');
         main_wrap.toggleClass('active');
         switch_btn.toggleClass('active');
-        if (switch_btn.hasClass('active')) {
+        if(switch_btn.hasClass('active')){
             switch_btn.text(Translator.trans('layout.menu.collapse'));
             sidebar_wrap.removeClass('noactive');
             $.cookie(COOKIE_SIDEBAR, 1, {
@@ -22,19 +22,18 @@ $(document).ready(function () {
                 path: '/'
             });
         }
-        setTimeout(function () {
-            resize()
-        }, 300);
+        /*setTimeout( function(){ resize()}, 300);*/
+        setTimeout( function(){ $(window).resize();}, 300);
     });
-
-    /* спойлер для модулей на главной*/
-    $('.spoiler_links').on('mousedown', function () {
+    $('.selectpicker').selectpicker();
+    /* спойлер */
+    $('.spoiler_links').on('mousedown', function(){
         var spoiler_container = $(this).parents('.spoiler_container');
         var body = spoiler_container.find('.spoiler_body');
-        if (body.css("display") == "none") {
-            body
-                .hide('normal')
-                .toggle('normal');
+        if (body.css("display")=="none")
+        {
+            body.hide('normal');
+            body.toggle('normal');
             $.cookie(body.data('cookie'), 1, {
                 path: '/'
             });
@@ -44,41 +43,44 @@ $(document).ready(function () {
                 path: '/'
             });
         }
+        spoiler_container.toggleClass('noactive');
         return false;
-    });
+     });
 
-    /* клик на пункте меню главной */
-    $('.main_menu .item.parent a').on('click', function () {
+     /* клик на пункте меню главной */
+     $('.main_menu .item.parent a').on('click', function(){
         var item = $(this).parents('.parent');
-        if (item.hasClass('active')) {
+        if(item.hasClass('active')){
             item.removeClass('active');
         } else {
+            $('.main_menu .item.parent.active').removeClass('active');
+            resize();
             item.addClass('active');
         }
         resize();
-        setTimeout(resize(), 500);
+        setTimeout( resize(), 500);
         return false;
     });
 
 
-    jQuery(document).ready(function () {
+     jQuery(document).ready(function(){
         resize();
 
         show_menu_function();
-    });
+     });
 
-    function show_menu_function() {
-        var count = $('.main_menu .item').length;
-    //    console.log(count);
+     function show_menu_function(){
+        /*var count = $('.main_menu .item').length;
+        console.log(count);
         for (i = 0; i < count; i++) {
-    //        console.log(i);
-            setTimeout($('.main_menu .item:eq(' + i + ')').addClass('animate_start'), 700);
-        }
+            console.log(i);
+            setTimeout( $('.main_menu .item:eq('+i+')').addClass('animate_start'), 700);
+        }*/
     }
 
-    window.onresize = resize;
+     window.onresize = resize;
 
-    function resize() {
+     function resize(){
         /* меню на главной */
         $('.main_menu .item').height('auto');
         $('.main_menu .item .description').height('auto');
@@ -86,30 +88,52 @@ $(document).ready(function () {
         var maxHeightItem = 0;
         var maxHeightItemText = 0;
         var maxHeightDownDrop = 0;
-        $('.main_menu .item').each(function () {
-            if ($(this).height() > maxHeightItem) {
-                maxHeightItem = $(this).height();
-            }
+        $('.main_menu .item').each(function(){
+            if($(this).height() > maxHeightItem){ maxHeightItem = $(this).height();}
         });
-        $('.main_menu .item .description').each(function () {
-            if ($(this).height() > maxHeightItemText) {
-                maxHeightItemText = $(this).height();
-            }
+        $('.main_menu .item .description').each(function(){
+            if($(this).height() > maxHeightItemText){ maxHeightItemText = $(this).height();}
         });
-        $('.main_menu .main_dropdown_menu').each(function () {
-            if ($(this).height() > maxHeightDownDrop) {
-                maxHeightDownDrop = $(this).height();
-            }
+        $('.main_menu .main_dropdown_menu').each(function(){
+            if($(this).height() > maxHeightDownDrop){ maxHeightDownDrop = $(this).height();}
         });
         $('.main_menu .item').height(maxHeightItem);
         $('.main_menu .item .description').height(maxHeightItemText);
-        if ($('.main_menu .item.parent.active').length > 0) {
+        if( $('.main_menu .item.parent.active').length > 0 ){
             $('.main_menu .item.parent.active').css({'marginBottom': maxHeightDownDrop + 5});
         } else {
             $('.main_menu .item.parent').css({'marginBottom': '10px'});
             $('.main_menu .item').height('auto');
             $('.main_menu .item .description').height('auto');
         }
-    //    console.log('resize')
+        console.log('resize')
     }
+
+
+    /* скролбар */
+    $(function() {
+        $('.scroll-pane').each(
+            function() {
+                $(this).jScrollPane({
+                    showArrows: $(this).is('.arrow')
+                });
+                var api = $(this).data('jsp');
+                var throttleTimeout;
+                $(window).bind(
+                    'resize',
+                    function() {
+                        if (!throttleTimeout) {
+                            throttleTimeout = setTimeout(
+                                function() {
+                                    api.reinitialise();
+                                    throttleTimeout = null;
+                                },
+                                50
+                            );
+                        }
+                    }
+                );
+            }
+        )
+    });
 });

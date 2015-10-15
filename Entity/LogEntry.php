37f40f2a70433fbc27d9973bd57fce30904d6df9
igor <igor.sv.n@gmail.com>
2015-10-15 16:50:08 +0300

    Configure log admin section filter.

--- Entity/LogEntry.php
+++ Entity/LogEntry.php
@@ -30,6 +30,19 @@ class LogEntry extends AbstractLogEntry
 {
     const LOG_ENTRY_CLASS = __CLASS__;
 
+    const ACTION_CREATE = 'create';
+    const ACTION_REMOVE = 'remove';
+    const ACTION_UPDATE = 'update';
+
+    /**
+     * @var array
+     */
+    private static $actions = array(
+        self::ACTION_CREATE => 'log.entity.actions.create',
+        self::ACTION_REMOVE => 'log.entity.actions.remove',
+        self::ACTION_UPDATE => 'log.entity.actions.update',
+    );
+
     /**
      * @var object
      *
@@ -57,7 +70,15 @@ class LogEntry extends AbstractLogEntry
      */
     public function getActionName()
     {
-        return 'log.entity.actions.'.$this->action;
+        return isset(self::$actions[$this->action]) ? self::$actions[$this->action] : $this->action;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getActions()
+    {
+        return self::$actions;
     }
 
     /**

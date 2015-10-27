06d67045d43a975d3a83ae164d00488243265cd8
igor <igor.sv.n@gmail.com>
2015-10-27 12:01:50 +0300

    Implement master-slave inputs JS.

---
+++ Resources/scripts/master-slave-inputs.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.slave_input').each(function () {
+        var $slave = $(this);
+        var $slaveContainer = $slave.parents('.table_row').first();
+        var masterSelector = $slave.data('master');
+        var showOn = $slave.data('show-on').toString();
+
+        $slave.parents('form').first()
+            .on('change', masterSelector, function () {
+                $(this).val().toString() === showOn ? $slaveContainer.show() : $slaveContainer.hide();
+            })
+            .find(masterSelector).trigger('change');
+    });
+});

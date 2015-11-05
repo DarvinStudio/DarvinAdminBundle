d1287b857f9e243e4546ce1cc96e0e9fbccbb61a
igor <igor.sv.n@gmail.com>
2015-11-05 11:31:24 +0300

    Fix master-slave inputs JS.

--- Resources/scripts/master-slave-inputs.js
+++ Resources/scripts/master-slave-inputs.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
+    var toggleSlaveContainer = function ($slaveContainer, $master, showOn) {
+        $master.val().toString() === showOn ? $slaveContainer.show() : $slaveContainer.hide();
+    };
+
     $('.slave_input').each(function () {
         var $slave = $(this);
         var $slaveContainer = $slave.parents('.table_row').first();
         var masterSelector = $slave.data('master');
         var showOn = $slave.data('show-on').toString();
 
-        $slave.parents('form').first()
-            .on('change', masterSelector, function () {
-                $(this).val().toString() === showOn ? $slaveContainer.show() : $slaveContainer.hide();
-            })
-            .find(masterSelector).trigger('click');
+        var $form = $slave.parents('form').first();
+
+        toggleSlaveContainer($slaveContainer, $form.find(masterSelector), showOn);
+
+        $form.on('change', masterSelector, function () {
+            toggleSlaveContainer($slaveContainer, $(this), showOn);
+        });
     });
 });

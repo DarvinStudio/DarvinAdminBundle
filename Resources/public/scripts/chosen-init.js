edbe4d71a5221375b68973a8413ac2317a0078db
igor <igor.sv.n@gmail.com>
2016-03-31 11:25:01 +0300

    Fix Chosen results scrolling in Firefox.

--- Resources/public/scripts/chosen-init.js
+++ Resources/public/scripts/chosen-init.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
+    var SCROLL_SPEED = -1 !== navigator.userAgent.indexOf('Firefox') ? 10 : 1;
+
+    var searchResultsMousewheel = function (e) {
+        var delta;
+
+        if (e.originalEvent) {
+            delta = e.originalEvent.deltaY || -e.originalEvent.wheelDelta || e.originalEvent.detail;
+        }
+        if (delta == null) {
+            return null;
+        }
+
+        e.preventDefault();
+
+        return this.search_results.scrollTop(delta * (e.type === 'DOMMouseScroll' ? 40 : SCROLL_SPEED) + this.search_results.scrollTop());
+    };
+
     var init;
     (init = function (context) {
-        $(context || 'body').find('select:visible')
-            .chosen({
-                allow_single_deselect:     true,
-                no_results_text:           Translator.trans('chosen.no_results_text'),
-                placeholder_text_multiple: Translator.trans('chosen.placeholder_text_multiple'),
-                placeholder_text_single:   Translator.trans('chosen.placeholder_text_single'),
-                search_contains:           true
-            })
-            .change(function () {
-                $(this).trigger('chosen:updated');
-            });
+        $(context || 'body').find('select:visible').each(function () {
+            $(this)
+                .chosen({
+                    allow_single_deselect:     true,
+                    no_results_text:           Translator.trans('chosen.no_results_text'),
+                    placeholder_text_multiple: Translator.trans('chosen.placeholder_text_multiple'),
+                    placeholder_text_single:   Translator.trans('chosen.placeholder_text_single'),
+                    search_contains:           true
+                })
+                .change(function () {
+                    $(this).trigger('chosen:updated');
+                })
+                .data('chosen').search_results_mousewheel = searchResultsMousewheel;
+        });
     })();
 
     $(document)

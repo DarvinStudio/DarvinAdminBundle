755cc36a5687e92d186779a54070c2df4f891dd8
igsvn <igor.sv.n@gmail.com>
2016-07-15 12:58:31 +0300

    Implement simple menu.

--- Menu/Item.php
+++ Menu/Item.php
@@ -18,21 +18,138 @@ class Item
     /**
      * @var string
      */
-    private $title;
+    private $name;
 
     /**
-     * @param string $title Title
+     * @var string
      */
-    public function __construct($title)
+    private $indexTitle;
+
+    /**
+     * @var string
+     */
+    private $newTitle;
+
+    /**
+     * @var string
+     */
+    private $indexUrl;
+
+    /**
+     * @var string
+     */
+    private $newUrl;
+
+    /**
+     * @var int
+     */
+    private $position;
+
+    /**
+     * @param string $name       Name
+     * @param string $indexTitle Index action title
+     */
+    public function __construct($name, $indexTitle)
     {
-        $this->title = $title;
+        $this->name = $name;
+        $this->indexTitle = $indexTitle;
+
+        $this->indexUrl = '#';
     }
 
     /**
      * @return string
      */
-    public function getTitle()
+    public function getName()
     {
-        return $this->title;
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIndexTitle()
+    {
+        return $this->indexTitle;
+    }
+
+    /**
+     * @param string $newTitle newTitle
+     *
+     * @return Item
+     */
+    public function setNewTitle($newTitle)
+    {
+        $this->newTitle = $newTitle;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewTitle()
+    {
+        return $this->newTitle;
+    }
+
+    /**
+     * @param string $indexUrl indexUrl
+     *
+     * @return Item
+     */
+    public function setIndexUrl($indexUrl)
+    {
+        $this->indexUrl = $indexUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIndexUrl()
+    {
+        return $this->indexUrl;
+    }
+
+    /**
+     * @param string $newUrl newUrl
+     *
+     * @return Item
+     */
+    public function setNewUrl($newUrl)
+    {
+        $this->newUrl = $newUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewUrl()
+    {
+        return $this->newUrl;
+    }
+
+    /**
+     * @param int $position position
+     *
+     * @return Item
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition()
+    {
+        return $this->position;
     }
 }
--- Menu/ItemFactory.php
+++ Menu/ItemFactory.php
@@ -10,7 +10,11 @@
 
 namespace Darvin\AdminBundle\Menu;
 
+use Darvin\AdminBundle\Metadata\Metadata;
 use Darvin\AdminBundle\Metadata\MetadataManager;
+use Darvin\AdminBundle\Route\AdminRouter;
+use Darvin\AdminBundle\Security\Permissions\Permission;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
  * Menu item factory
@@ -18,15 +22,32 @@ use Darvin\AdminBundle\Metadata\MetadataManager;
 class ItemFactory implements ItemFactoryInterface
 {
     /**
+     * @var \Darvin\AdminBundle\Route\AdminRouter
+     */
+    private $adminRouter;
+
+    /**
+     * @var \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface
+     */
+    private $authorizationChecker;
+
+    /**
      * @var \Darvin\AdminBundle\Metadata\MetadataManager
      */
     private $metadataManager;
 
     /**
-     * @param \Darvin\AdminBundle\Metadata\MetadataManager $metadataManager Metadata manager
+     * @param \Darvin\AdminBundle\Route\AdminRouter                                        $adminRouter          Admin router
+     * @param \Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface $authorizationChecker Authorization checker
+     * @param \Darvin\AdminBundle\Metadata\MetadataManager                                 $metadataManager      Metadata manager
      */
-    public function __construct(MetadataManager $metadataManager)
-    {
+    public function __construct(
+        AdminRouter $adminRouter,
+        AuthorizationCheckerInterface $authorizationChecker,
+        MetadataManager $metadataManager
+    ) {
+        $this->adminRouter = $adminRouter;
+        $this->authorizationChecker = $authorizationChecker;
         $this->metadataManager = $metadataManager;
     }
 
@@ -38,9 +59,41 @@ class ItemFactory implements ItemFactoryInterface
         $items = [];
 
         foreach ($this->metadataManager->getAllMetadata() as $meta) {
-            $items[] = new Item($meta->getBaseTranslationPrefix().'action.index.link');
+            $config = $meta->getConfiguration();
+
+            if (!$meta->hasParent() && !$config['menu']['skip']) {
+                $items[] = $this->createItemFromMetadata($meta);
+            }
         }
 
         return $items;
+    }
+
+    /**
+     * @param \Darvin\AdminBundle\Metadata\Metadata $meta Metadata
+     *
+     * @return \Darvin\AdminBundle\Menu\Item
+     */
+    private function createItemFromMetadata(Metadata $meta)
+    {
+        $config = $meta->getConfiguration();
+        $entityClass = $meta->getEntityClass();
+
+        $item = (new Item($meta->getEntityName(), $meta->getBaseTranslationPrefix().'action.index.link'))
+            ->setNewTitle($meta->getBaseTranslationPrefix().'action.new.link')
+            ->setPosition($config['menu']['position']);
+
+        if ($this->authorizationChecker->isGranted(Permission::VIEW, $entityClass)
+            && $this->adminRouter->isRouteExists($entityClass, AdminRouter::TYPE_INDEX)
+        ) {
+            $item->setIndexUrl($this->adminRouter->generate(null, $entityClass, AdminRouter::TYPE_INDEX));
+        }
+        if ($this->authorizationChecker->isGranted(Permission::CREATE_DELETE, $entityClass)
+            && $this->adminRouter->isRouteExists($entityClass, AdminRouter::TYPE_NEW)
+        ) {
+            $item->setNewUrl($this->adminRouter->generate(null, $entityClass, AdminRouter::TYPE_NEW));
+        }
+
+        return $item;
     }
 }

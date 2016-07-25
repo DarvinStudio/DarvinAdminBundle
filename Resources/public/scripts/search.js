24d1c2bc0329c866196988188bfbe97f0f38aa95
igor <igor.sv.n@gmail.com>
2016-07-25 10:01:42 +0300

    Search using AJAX.

---
+++ Resources/public/scripts/search.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var $results = $('.search_results[data-source]');
+
+    if (!$results.length) {
+        return;
+    }
+
+    var pending = false;
+
+    $results.each(function () {
+        var $results = $(this);
+
+        var interval = setInterval(function () {
+            if (pending) {
+                return;
+            }
+
+            pending = true;
+            $results.html(AJAX_LOADER);
+
+            $.ajax({
+                url: $results.data('source')
+            }).done(function (html) {
+                $results.html(html);
+            }).complete(function () {
+                clearInterval(interval);
+                pending = false;
+            }).error(onAjaxError);
+        }, 100);
+    });
+});

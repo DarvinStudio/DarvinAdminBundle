7aec66a2f4d381d717da182a10eee604cd37a8b1
igor <igor.sv.n@gmail.com>
2016-09-16 09:24:06 +0300

    Replace Fancybox with Colorbox.

---
+++ Resources/public/scripts/colorbox-init.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $.colorbox.settings = $.extend({}, $.colorbox.settings, {
+        maxWidth:  '90%',
+        opacity:   0.7,
+        scrolling: false
+    });
+
+    var translatable = [
+        'current',
+        'previous',
+        'next',
+        'close',
+        'xhrError',
+        'imgError',
+        'slideshowStart',
+        'slideshowStop'
+    ];
+    var locale = {};
+
+    for (var i = 0; i < translatable.length; i++) {
+        locale[translatable[i]] = Translator.trans('colorbox.' + translatable[i]);
+    }
+
+    var init;
+    (init = function (context) {
+        $(context)
+            .on('click', 'a.colorbox_ajax[href]', function (e) {
+                e.preventDefault();
+
+                var $link = $(this);
+
+                var url = $link.attr('href');
+
+                if (!url || '#' === url) {
+                    return;
+                }
+
+                $.colorbox($.extend({}, locale, {
+                    href:  url,
+                    title: $link.attr('title')
+                }));
+            })
+            .find('.colorbox').colorbox(locale);
+    })('body');
+
+
+    $(document).on('searchComplete', function (e, results) {
+        init(results);
+    });
+});

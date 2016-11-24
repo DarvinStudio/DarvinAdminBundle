58b3bdb6c260893187712f05a1b6172201eb7ce4
igor <igor.sv.n@gmail.com>
2016-11-24 12:40:50 +0300

    Implement translatable form type.

---
+++ Form/Type/TranslatableType.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2016, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Translatable form type
+ */
+class TranslatableType extends AbstractType
+{
+    const TRANSLATABLE_TYPE_CLASS = __CLASS__;
+
+    /**
+     * @var string[]
+     */
+    private $locales;
+
+    /**
+     * @param string[] $locales Locales
+     */
+    public function __construct(array $locales)
+    {
+        $this->locales = $locales;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        foreach ($this->locales as $locale) {
+            $builder->add($locale, $options['type']);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefault('type', 'Symfony\Component\Form\Extension\Core\Type\TextType')
+            ->setAllowedTypes('type', 'string');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'darvin_admin_translatable';
+    }
+}

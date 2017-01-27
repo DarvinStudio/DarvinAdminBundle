3ef35c68816bbeb3e89cbb85a04dde59fee4b878
igor <igor.sv.n@gmail.com>
2017-01-27 15:40:24 +0300

    Bump version.

--- DarvinAdminBundle.php
+++ DarvinAdminBundle.php
@@ -27,7 +27,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class DarvinAdminBundle extends Bundle
 {
-    const VERSION = '5.1.1';
+    const VERSION = '5.1.2';
 
     /**
      * {@inheritdoc}

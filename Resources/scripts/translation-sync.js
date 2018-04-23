6592184c4adc41138df98af00cb270fd2f4b3e0b
igor <igor.sv.n@gmail.com>
2018-04-23 10:41:51 +0300

    Translation sync JS: translate all fields (not only required).

--- Resources/scripts/translation-sync.js
+++ Resources/scripts/translation-sync.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
     var getLocale = function ($tab) {
         var matches = $tab.attr('class').match(/a2lix_translationsfields-([a-z]+)\s*/i);
 
@@ -6,17 +6,20 @@ $(document).ready(function () {
     };
 
     $('body')
-        .on('change', 'form .a2lix_translationsFields .tab-pane.active input[type="text"][required="required"]', function () {
+        .on('click', 'form .a2lix_translationsLocales.nav.nav-tabs a', function () {
+            $(this).parents('.a2lix_translations').first().find('input[type="text"]').removeData('synced');
+        })
+        .on('change', 'form .a2lix_translationsFields .tab-pane.active input[type="text"]', function () {
             var $sourceInput = $(this);
 
             $sourceInput.removeData('synced');
 
             var $row = $sourceInput.parents('.table_row').first();
-            var $sourceTab = $row.parents('.tab-pane').first();
 
-            var rowIndex = $row.index();
+            var $sourceTab = $row.parents('.tab-pane').first(),
+                rowIndex   = $row.index(),
+                sourceText = $sourceInput.val();
 
-            var sourceText = $sourceInput.val();
             var sourceLocale = getLocale($sourceTab);
 
             $sourceTab.siblings('.tab-pane').each(function () {
@@ -37,8 +40,5 @@ $(document).ready(function () {
                     $targetInput.val(translated).data('synced', true);
                 });
             });
-        })
-        .on('click', 'form .a2lix_translationsLocales.nav.nav-tabs a', function () {
-            $(this).parents('.a2lix_translations').first().find('input[type="text"][required="required"]').removeData('synced');
         });
 });

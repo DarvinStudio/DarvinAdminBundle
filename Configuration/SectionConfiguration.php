52d4d74854e3468677cea2b08664662363e2b566
Igor Nikolaev <igor.sv.n@gmail.com>
2018-10-22 16:23:26 +0300

    Refactor section configuration.

--- Configuration/SectionConfiguration.php
+++ Configuration/SectionConfiguration.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
  * @copyright Copyright (c) 2016, Darvin Studio
@@ -11,6 +11,7 @@
 namespace Darvin\AdminBundle\Configuration;
 
 use Darvin\Utils\ObjectNamer\ObjectNamerInterface;
+use Darvin\Utils\ORM\EntityResolverInterface;
 
 /**
  * Section configuration
@@ -18,61 +19,52 @@ use Darvin\Utils\ObjectNamer\ObjectNamerInterface;
 class SectionConfiguration
 {
     /**
-     * @var array
+     * @var \Darvin\Utils\ORM\EntityResolverInterface
      */
-    private $sectionByAliases;
+    protected $entityResolver;
+
+    /**
+     * @var \Darvin\Utils\ObjectNamer\ObjectNamerInterface
+     */
+    protected $objectNamer;
 
     /**
      * @var array
      */
-    private $sectionByEntities;
+    protected $config;
 
     /**
+     * @var \Darvin\AdminBundle\Configuration\Section[]|null
+     */
+    protected $sections;
+
+    /**
+     * @param \Darvin\Utils\ORM\EntityResolverInterface      $entityResolver Entity resolver
      * @param \Darvin\Utils\ObjectNamer\ObjectNamerInterface $objectNamer    Object namer
-     * @param array[]                                        $configs        Configs
-     * @param array                                          $entityOverride Entity override configuration
-     *
-     * @throws \Darvin\AdminBundle\Configuration\ConfigurationException
+     * @param array                                          $config         Configuration
      */
-    public function __construct(ObjectNamerInterface $objectNamer, array $configs, array $entityOverride)
+    public function __construct(EntityResolverInterface $entityResolver, ObjectNamerInterface $objectNamer, array $config)
     {
-        $this->sectionByAliases = $this->sectionByEntities = [];
+        $this->entityResolver = $entityResolver;
+        $this->objectNamer = $objectNamer;
+        $this->config = $config;
 
-        foreach ($configs as $config) {
-            if (!$config['enabled']) {
-                continue;
-            }
-
-            $entity = $config['entity'];
-            $alias = !empty($config['alias']) ? $config['alias'] : $objectNamer->name($entity);
-
-            if (isset($this->sectionByEntities[$entity])) {
-                throw new ConfigurationException(sprintf('Section for entity "%s" already exists.', $entity));
-            }
-            if (isset($this->sectionByAliases[$alias])) {
-                throw new ConfigurationException(sprintf('Section with alias "%s" already exists.', $alias));
-            }
-            if (isset($entityOverride[$entity])) {
-                $entity = $entityOverride[$entity];
-            }
-
-            $this->sectionByEntities[$entity] = new Section($alias, $entity, $config['config']);
-        }
+        $this->sections = null;
     }
 
     /**
      * @param string $entity Entity class
      *
      * @return \Darvin\AdminBundle\Configuration\Section
-     * @throws \Darvin\AdminBundle\Configuration\ConfigurationException
+     * @throws \InvalidArgumentException
      */
-    public function getSection($entity)
+    public function getSection(string $entity): Section
     {
         if (!$this->hasSection($entity)) {
-            throw new ConfigurationException(sprintf('Section for entity "%s" does not exist.', $entity));
+            throw new \InvalidArgumentException(sprintf('Section for entity "%s" does not exist.', $entity));
         }
 
-        return $this->sectionByEntities[$entity];
+        return $this->getSections()[$entity];
     }
 
     /**
@@ -80,16 +72,39 @@ class SectionConfiguration
      *
      * @return bool
      */
-    public function hasSection($entity)
+    public function hasSection(string $entity): bool
     {
-        return isset($this->sectionByEntities[$entity]);
+        $sections = $this->getSections();
+
+        return isset($sections[$entity]);
     }
 
     /**
      * @return \Darvin\AdminBundle\Configuration\Section[]
      */
-    public function getSections()
+    public function getSections(): array
     {
-        return $this->sectionByEntities;
+        if (null === $this->sections) {
+            $this->sections = [];
+
+            foreach ($this->config as $params) {
+                if (!$params['enabled']) {
+                    continue;
+                }
+
+                $alias  = $params['alias'];
+                $entity = $params['entity'];
+
+                if (empty($alias)) {
+                    $alias = $this->objectNamer->name($entity);
+                }
+
+                $resolvedEntity = $this->entityResolver->resolve($entity);
+
+                $this->sections[$resolvedEntity] = new Section($alias, $resolvedEntity, $params['config']);
+            }
+        }
+
+        return $this->sections;
     }
 }

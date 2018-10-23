1e5ccf2f8e9a98dfbe050734372c46a6a55786af
Igor Nikolaev <igor.sv.n@gmail.com>
2018-10-23 09:35:48 +0300

    Use entity resolver in entity namer.

--- EntityNamer/EntityNamer.php
+++ EntityNamer/EntityNamer.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
  * @copyright Copyright (c) 2016, Darvin Studio
@@ -12,7 +12,7 @@ namespace Darvin\AdminBundle\EntityNamer;
 
 use Darvin\AdminBundle\Configuration\SectionConfiguration;
 use Darvin\Utils\ObjectNamer\ObjectNamerInterface;
-use Doctrine\Common\Util\ClassUtils;
+use Darvin\Utils\ORM\EntityResolverInterface;
 
 /**
  * Entity namer
@@ -20,35 +20,35 @@ use Doctrine\Common\Util\ClassUtils;
 class EntityNamer implements EntityNamerInterface
 {
     /**
+     * @var \Darvin\Utils\ORM\EntityResolverInterface
+     */
+    protected $entityResolver;
+
+    /**
      * @var \Darvin\Utils\ObjectNamer\ObjectNamerInterface
      */
-    private $objectNamer;
+    protected $objectNamer;
 
     /**
      * @var \Darvin\AdminBundle\Configuration\SectionConfiguration
      */
-    private $sectionConfig;
+    protected $sectionConfig;
 
     /**
      * @var array
      */
-    private $entityOverride;
+    protected $names;
 
     /**
-     * @var string[]
-     */
-    private $names;
-
-    /**
+     * @param \Darvin\Utils\ORM\EntityResolverInterface              $entityResolver Entity resolver
      * @param \Darvin\Utils\ObjectNamer\ObjectNamerInterface         $objectNamer    Object namer
      * @param \Darvin\AdminBundle\Configuration\SectionConfiguration $sectionConfig  Section configuration
-     * @param array                                                  $entityOverride Entity override configuration
      */
-    public function __construct(ObjectNamerInterface $objectNamer, SectionConfiguration $sectionConfig, array $entityOverride)
+    public function __construct(EntityResolverInterface $entityResolver, ObjectNamerInterface $objectNamer, SectionConfiguration $sectionConfig)
     {
+        $this->entityResolver = $entityResolver;
         $this->objectNamer = $objectNamer;
         $this->sectionConfig = $sectionConfig;
-        $this->entityOverride = $entityOverride;
 
         $this->names = [];
     }
@@ -56,9 +56,9 @@ class EntityNamer implements EntityNamerInterface
     /**
      * {@inheritdoc}
      */
-    public function name($entityOrClass)
+    public function name($entity): string
     {
-        $class = is_object($entityOrClass) ? ClassUtils::getClass($entityOrClass) : $entityOrClass;
+        $class = is_object($entity) ? get_class($entity) : $entity;
 
         if (!isset($this->names[$class])) {
             $this->names[$class] = $this->getName($class);
@@ -72,13 +72,10 @@ class EntityNamer implements EntityNamerInterface
      *
      * @return string
      */
-    private function getName($class)
+    final protected function getName(string $class): string
     {
-        $class = preg_replace('/(.*[^\\\]+)Interface$/', '$1', $class);
+        $class = $this->entityResolver->resolve($class);
 
-        if (isset($this->entityOverride[$class])) {
-            $class = $this->entityOverride[$class];
-        }
         if ($this->sectionConfig->hasSection($class)) {
             return $this->sectionConfig->getSection($class)->getAlias();
         }

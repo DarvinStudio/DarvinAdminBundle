cb71cc56414f8ed6568c30cc16d1f58c368a4632
Igor Nikolaev <igor.sv.n@gmail.com>
2018-11-16 08:47:04 +0300

    Refactor abstract CKEditor widget.

--- CKEditor/AbstractCKEditorWidget.php
+++ CKEditor/AbstractCKEditorWidget.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
  * @copyright Copyright (c) 2016, Darvin Studio
@@ -21,13 +21,13 @@ abstract class AbstractCKEditorWidget extends AbstractWidget
     /**
      * {@inheritdoc}
      */
-    protected function configureOptions(OptionsResolver $resolver)
+    protected function configureOptions(OptionsResolver $resolver): void
     {
         parent::configureOptions($resolver);
 
         $resolver->setDefaults([
             'icon'  => __DIR__.'/../Resources/images/ckeditor_stub.png',
-            'title' => 'ckeditor_widget.'.$this->getName(),
+            'title' => sprintf('ckeditor_widget.%s', $this->getName()),
         ]);
     }
 }

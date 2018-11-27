8dcdf80f8e9446aa24e0ae74b410cdf3508ef34a
Igor Nikolaev <igor.sv.n@gmail.com>
2018-11-27 16:46:51 +0300

    Refactor Yandex translator JS.

--- Resources/scripts/yandex-translator.js
+++ Resources/scripts/yandex-translator.js
@@ -1,19 +1,19 @@
-var YandexTranslator = (function () {
-    var key = $('body').data('trans-api-key');
+const YandexTranslator = (() => {
+    const KEY = $('body').data('trans-api-key');
 
     return {
-        translate: function (text, from, to, successCallback) {
-            if (!key) {
+        translate: (text, from, to, successCallback) => {
+            if (!KEY) {
                 successCallback(text);
 
                 return;
             }
 
-            var lang = [from, to].join('-');
+            let lang = [from, to].join('-');
 
             $.ajax({
-                url: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + key + '&text=' + text + '&lang=' + lang
-            }).done(function (response) {
+                url: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + KEY + '&text=' + text + '&lang=' + lang
+            }).done((response) => {
                 if (200 !== response.code) {
                     console.log(response);
 

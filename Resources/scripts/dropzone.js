b638bf3bd4d6b5ea730af83c5ada9267ce47cf78
Igor Nikolaev <igor.sv.n@gmail.com>
2018-12-18 15:06:44 +0300

    Refactor Dropzone JS.

--- Resources/scripts/dropzone.js
+++ Resources/scripts/dropzone.js
@@ -1,74 +1,72 @@
 Dropzone.autoDiscover = false;
 
-$(function () {
-    var init;
-    (init = function (context) {
-        $(context || 'body').find('.dropzone[data-files][data-url]').each(function () {
-            var $dropzone = $(this);
+$(document).on('app.html', (e, args) => {
+    args.$html.find('.dropzone[data-files][data-url]').each((i, dropzone) => {
+        let $dropzone = $(dropzone);
 
-            var options = $dropzone.data();
+        let options = $dropzone.data();
 
-            var $files = $dropzone.closest('form').find('#' + options.files + '[data-prototype]');
+        let $files = $dropzone.closest('form').find('#' + options.files + '[data-prototype]');
 
-            var filePrototype = $files.data('prototype');
+        let filePrototype = $files.data('prototype');
 
-            var defaultMessage = Translator.trans('dropzone.default_message');
+        let defaultMessage = Translator.trans('dropzone.default_message');
 
-            if (options.description) {
-                defaultMessage += '<br><br><b>' + Translator.trans(options.description) + '</b>';
+        if (options.description) {
+            defaultMessage += '<br><br><b>' + Translator.trans(options.description) + '</b>';
+        }
+
+        new Dropzone('#' + $dropzone.attr('id'), {
+            acceptedFiles:       options.acceptedFiles,
+            addRemoveLinks:      true,
+            dictDefaultMessage:  defaultMessage,
+            dictFileTooBig:      Translator.trans('dropzone.file_too_big'),
+            dictInvalidFileType: Translator.trans('dropzone.invalid_file_type'),
+            dictRemoveFile:      Translator.trans('dropzone.remove_file'),
+            filesizeBase:        1024,
+            maxFilesize:         options.maxFilesize,
+            url:                 options.url,
+            accept:              (file, done) => {
+                file.acceptDimensions = done;
+                file.rejectDimensions = () => {
+                    done(Translator.trans('dropzone.reject_dimensions', {
+                        max_width:  options.constraintMaxWidth,
+                        max_height: options.constraintMaxHeight
+                    }));
+                };
+            },
+            // Must be "function"!
+            init: function () {
+                this
+                    .on('removedfile', (file) => {
+                        $files.find('.original_filename[value="' + file.name + '"]').parents('.table_row:first').remove();
+                    })
+                    .on('success', (file, response) => {
+                        let $file = $(filePrototype.replace(/__name__/g, $files.children().length));
+
+                        $file.find('.filename').val(response[0]);
+                        $file.find('.original_filename').val(file.name);
+
+                        $files.append($file);
+                    })
+                    .on('thumbnail', (file) => {
+                        let interval = setInterval(() => {
+                            if ('undefined' === typeof file.acceptDimensions || 'undefined' === typeof file.rejectDimensions) {
+                                return;
+                            }
+
+                            clearInterval(interval);
+
+                            if (file.width > options.constraintMaxWidth || file.height > options.constraintMaxHeight) {
+                                file.rejectDimensions();
+
+                                return;
+                            }
+
+                            file.acceptDimensions();
+                        }, 100);
+                    });
             }
-
-            new Dropzone('#' + $dropzone.attr('id'), {
-                acceptedFiles:       options.acceptedFiles,
-                addRemoveLinks:      true,
-                dictDefaultMessage:  defaultMessage,
-                dictFileTooBig:      Translator.trans('dropzone.file_too_big'),
-                dictInvalidFileType: Translator.trans('dropzone.invalid_file_type'),
-                dictRemoveFile:      Translator.trans('dropzone.remove_file'),
-                filesizeBase:        1024,
-                maxFilesize:         options.maxFilesize,
-                url:                 options.url,
-                accept:              function (file, done) {
-                    file.acceptDimensions = done;
-                    file.rejectDimensions = function () {
-                        done(Translator.trans('dropzone.reject_dimensions', {
-                            max_width:  options.constraintMaxWidth,
-                            max_height: options.constraintMaxHeight
-                        }));
-                    };
-                },
-                init: function () {
-                    this
-                        .on('removedfile', function (file) {
-                            $files.find('.original_filename[value="' + file.name + '"]').parents('.table_row:first').remove();
-                        })
-                        .on('success', function (file, response) {
-                            var $file = $(filePrototype.replace(/__name__/g, $files.children().length));
-
-                            $file.find('.filename').val(response[0]);
-                            $file.find('.original_filename').val(file.name);
-
-                            $files.append($file);
-                        })
-                        .on('thumbnail', function (file) {
-                            var interval = setInterval(function () {
-                                if ('undefined' === typeof file.acceptDimensions || 'undefined' === typeof file.rejectDimensions) {
-                                    return;
-                                }
-
-                                clearInterval(interval);
-
-                                if (file.width > options.constraintMaxWidth || file.height > options.constraintMaxHeight) {
-                                    file.rejectDimensions();
-
-                                    return;
-                                }
-
-                                file.acceptDimensions();
-                            }, 100);
-                        });
-                }
-            });
         });
-    })();
+    });
 });

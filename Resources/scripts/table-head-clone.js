657269cfad376dec0a089e59657c8b6672d9fedb
Igor Nikolaev <igor.sv.n@gmail.com>
2018-12-18 16:09:48 +0300

    Make table head clone JS persistent.

--- Resources/scripts/table-head-clone.js
+++ Resources/scripts/table-head-clone.js
@@ -1,32 +1,34 @@
-$('.section_table').each(function(){
-	var self = $(this);
-	var screenHeight = window.innerHeight;
-	var tableHeight = $(this).height();
-	var head = $(this).find('thead tr').eq(0).clone();
-	
-	if(tableHeight > screenHeight*1.8){
-		var counterHeight = 0;
-		var countHead = tableHeight/( screenHeight*0.9);
-		var interval = tableHeight / countHead;
-		var intervalCurr = 0;
-		var rows = self.find('tbody tr');
-		
-		for(var i = 0; i < rows.length; i++){
-			intervalCurr += rows.eq(i).height();
-			
-			if(intervalCurr >= interval){
-				$(head).clone().insertAfter(rows.eq(i));
-				intervalCurr = 0;
-			}
-		}
-	} 
-	
-	setTimeout(function(){
-		if(tableHeight > screenHeight*1.2){
-			if(self.find('tr:last th')){
-				$(this).find('table tbody').append( head.clone() );
-			}
-		}
-	}, 10)
-	
+$(document).on('app.html', (e, args) => {
+    args.$html.find('.section_table').each(function(){
+        var self = $(this);
+        var screenHeight = window.innerHeight;
+        var tableHeight = $(this).height();
+        var head = $(this).find('thead tr').eq(0).clone();
+
+        if(tableHeight > screenHeight*1.8){
+            var counterHeight = 0;
+            var countHead = tableHeight/( screenHeight*0.9);
+            var interval = tableHeight / countHead;
+            var intervalCurr = 0;
+            var rows = self.find('tbody tr');
+
+            for(var i = 0; i < rows.length; i++){
+                intervalCurr += rows.eq(i).height();
+
+                if(intervalCurr >= interval){
+                    $(head).clone().insertAfter(rows.eq(i));
+                    intervalCurr = 0;
+                }
+            }
+        }
+
+        setTimeout(function(){
+            if(tableHeight > screenHeight*1.2){
+                if(self.find('tr:last th')){
+                    $(this).find('table tbody').append( head.clone() );
+                }
+            }
+        }, 10)
+
+    });
 });

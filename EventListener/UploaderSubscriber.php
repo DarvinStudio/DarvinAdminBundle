f0a7ca760691290e53017bd1bf5d4fc6f9438f1f
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-18 11:37:03 +0300

    Replace event listeners with subscribers.

--- EventListener/UploaderSubscriber.php
+++ EventListener/UploaderSubscriber.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2015, Darvin Studio
+ * @copyright Copyright (c) 2015-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
@@ -12,17 +12,29 @@ namespace Darvin\AdminBundle\EventListener;
 
 use Oneup\UploaderBundle\Event\PostUploadEvent;
 use Oneup\UploaderBundle\Uploader\Response\AbstractResponse;
+use Oneup\UploaderBundle\UploadEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\File\File;
 
 /**
- * Uploader listener
+ * Uploader event subscriber
  */
-class UploaderListener
+class UploaderSubscriber implements EventSubscriberInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            UploadEvents::POST_UPLOAD => 'postUpload',
+        ];
+    }
+
     /**
      * @param \Oneup\UploaderBundle\Event\PostUploadEvent $event Event
      */
-    public function postUpload(PostUploadEvent $event)
+    public function postUpload(PostUploadEvent $event): void
     {
         $response = $event->getResponse();
 

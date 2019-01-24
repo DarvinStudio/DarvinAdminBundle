ce9b3131bb0027c530b8083dd3ed1f6916e267c7
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-24 08:54:09 +0300

    Move submission in form JS to separate function.

--- Resources/scripts/form.js
+++ Resources/scripts/form.js
@@ -3,6 +3,11 @@
         form: 'form.js-form'
     };
 
+    const replaceUrl = (url) => {
+        if (url && 'undefined' !== typeof history) {
+            history.pushState(null, null, url);
+        }
+    };
     const replaceContent = (html) => {
         let $content = $('#js-content');
 
@@ -26,10 +31,108 @@
 
         return true;
     };
-    const replaceUrl = (url) => {
-        if (url && 'undefined' !== typeof history) {
-            history.pushState(null, null, url);
+    const submit = (form) => {
+        let $form = $(form);
+
+        let options = $form.data();
+
+        if (options.submitted || options.redirecting) {
+            return;
         }
+
+        $form.data('submitted', true);
+
+        App.startPreloading();
+
+        let url  = $form.attr('action') || options.url || '',
+            type = ($form.attr('method') || 'get').toLowerCase(),
+            xhr  = new XMLHttpRequest();
+
+        let params = {
+            url:  url,
+            type: type,
+            xhr:  () => {
+                return xhr;
+            }
+        };
+
+        if ('get' === type || 'undefined' === typeof FormData) {
+            params.data = $form.serialize();
+        } else {
+            $.extend(params, {
+                data:        new FormData($form[0]),
+                contentType: false,
+                processData: false
+            });
+        }
+
+        $.ajax(params).done((data) => {
+            if ('get' === type) {
+                let parts = [location.pathname];
+
+                if (params.data) {
+                    parts.push('?', params.data);
+                }
+
+                replaceUrl(parts.join(''));
+            }
+            if (!$.isPlainObject(data)) {
+                if (replaceContent(data)) {
+                    return;
+                }
+
+                document.open();
+                document.write(data);
+                document.close();
+
+                replaceUrl(xhr.responseURL);
+
+                return;
+            }
+
+            App.notify(data.message, data.success ? 'success' : 'error');
+
+            if (data.success && options.reloadPage && !$('#js-content').data('not-reloadable')) {
+                App.startPreloading('form');
+
+                let pageUrl = data.redirectUrl || '';
+
+                $.ajax({
+                    url:   pageUrl,
+                    cache: false
+                }).done(replaceContent).always(() => {
+                    replaceUrl(pageUrl);
+                }).always(() => {
+                    App.stopPreloading('form');
+                }).fail(App.onAjaxFail);
+
+                return;
+            }
+            if (App.redirect(data.redirectUrl, !data.message)) {
+                $form.data('redirecting', true);
+            }
+            if (data.html) {
+                let $html   = $(data.html),
+                    $target = $form;
+
+                if (options.target) {
+                    $target = $form.closest(options.target);
+
+                    if (!$target.length) {
+                        $target = $(options.target + ':first');
+                    }
+                }
+                if ($target.length) {
+                    $target.replaceWith($html);
+
+                    $(document).trigger('app.html', {
+                        $html: $html.parent()
+                    });
+                }
+            }
+        }).always(() => {
+            $form.removeData('submitted');
+        }).fail(App.onAjaxFail);
     };
 
     $('body')
@@ -41,106 +144,6 @@
         .on('submit', SELECTORS.form, (e) => {
             e.preventDefault();
 
-            let $form = $(e.currentTarget);
-
-            let options = $form.data();
-
-            if (options.submitted || options.redirecting) {
-                return;
-            }
-
-            $form.data('submitted', true);
-
-            App.startPreloading();
-
-            let url  = $form.attr('action') || options.url || '',
-                type = ($form.attr('method') || 'get').toLowerCase(),
-                xhr  = new XMLHttpRequest();
-
-            let params = {
-                url:  url,
-                type: type,
-                xhr:  () => {
-                    return xhr;
-                }
-            };
-
-            if ('get' === type || 'undefined' === typeof FormData) {
-                params.data = $form.serialize();
-            } else {
-                $.extend(params, {
-                    data:        new FormData($form[0]),
-                    contentType: false,
-                    processData: false
-                });
-            }
-
-            $.ajax(params).done((data) => {
-                if ('get' === type) {
-                    let parts = [location.pathname];
-
-                    if (params.data) {
-                        parts.push('?', params.data);
-                    }
-
-                    replaceUrl(parts.join(''));
-                }
-                if (!$.isPlainObject(data)) {
-                    if (replaceContent(data)) {
-                        return;
-                    }
-
-                    document.open();
-                    document.write(data);
-                    document.close();
-
-                    replaceUrl(xhr.responseURL);
-
-                    return;
-                }
-
-                App.notify(data.message, data.success ? 'success' : 'error');
-
-                if (data.success && options.reloadPage && !$('#js-content').data('not-reloadable')) {
-                    App.startPreloading('form');
-
-                    let pageUrl = data.redirectUrl || '';
-
-                    $.ajax({
-                        url:   pageUrl,
-                        cache: false
-                    }).done(replaceContent).always(() => {
-                        replaceUrl(pageUrl);
-                    }).always(() => {
-                        App.stopPreloading('form');
-                    }).fail(App.onAjaxFail);
-
-                    return;
-                }
-                if (App.redirect(data.redirectUrl, !data.message)) {
-                    $form.data('redirecting', true);
-                }
-                if (data.html) {
-                    let $html   = $(data.html),
-                        $target = $form;
-
-                    if (options.target) {
-                        $target = $form.closest(options.target);
-
-                        if (!$target.length) {
-                            $target = $(options.target + ':first');
-                        }
-                    }
-                    if ($target.length) {
-                        $target.replaceWith($html);
-
-                        $(document).trigger('app.html', {
-                            $html: $html.parent()
-                        });
-                    }
-                }
-            }).always(() => {
-                $form.removeData('submitted');
-            }).fail(App.onAjaxFail);
-    });
+            submit(e.currentTarget);
+        });
 })();

276d165c85b8979d771b6d5ddecbbc1a8732224c
Igor Nikolaev <igor.sv.n@gmail.com>
2019-03-11 16:38:12 +0300

    Implement elFinder text form type.

---
+++ Form/Type/ElFinderTextType.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+/**
+ * elFinder text form type
+ */
+class ElFinderTextType extends AbstractType
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getParent(): string
+    {
+        return TextType::class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'darvin_admin_el_finder_text';
+    }
+}

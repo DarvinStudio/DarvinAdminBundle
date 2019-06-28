81568c121c1bb2d5ed7ba9641822a70ce72b9dd4
Igor Nikolaev <igor.sv.n@gmail.com>
2019-06-28 16:15:13 +0300

    Implement page controller.

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -13,12 +13,33 @@ namespace Darvin\AdminBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Environment;
 
 /**
  * Page controller
  */
 class PageController
 {
+    /**
+     * @var \Twig\Environment
+     */
+    private $twig;
+
+    /**
+     * @var string[]
+     */
+    private $fallbackLocales;
+
+    /**
+     * @param \Twig\Environment $twig            Twig
+     * @param string[]          $fallbackLocales Fallback locales
+     */
+    public function __construct(Environment $twig, array $fallbackLocales)
+    {
+        $this->twig = $twig;
+        $this->fallbackLocales = $fallbackLocales;
+    }
+
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request Request
      * @param string                                    $slug    Page slug
@@ -28,6 +49,16 @@ class PageController
      */
     public function __invoke(Request $request, string $slug): Response
     {
+        $loader = $this->twig->getLoader();
+
+        foreach (array_merge([$request->getLocale()], $this->fallbackLocales) as $locale) {
+            $template = sprintf('@DarvinAdmin/page/%s/%s.html.twig', $slug, $locale);
+
+            if ($loader->exists($template)) {
+                return new Response($this->twig->render($template));
+            }
+        }
+
         throw new NotFoundHttpException(sprintf('Page "%s" does not exist.', $slug));
     }
 }

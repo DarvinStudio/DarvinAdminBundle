27bc5d5cc9f776fdeec010c08681fccb2fa4124a
Igor Nikolaev <igor.sv.n@gmail.com>
2019-07-10 10:07:22 +0300

    Customize form theme.

--- Form/Type/TripleboxType.php
+++ Form/Type/TripleboxType.php
@@ -12,8 +12,6 @@ namespace Darvin\AdminBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -35,14 +33,6 @@ class TripleboxType extends AbstractType
                 'boolean.no'  => 0,
             ],
         ]);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function finishView(FormView $view, FormInterface $form, array $options): void
-    {
-        $view->vars['compound'] = false;
     }
 
     /**

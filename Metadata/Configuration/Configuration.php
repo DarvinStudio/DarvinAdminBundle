f7e7c7ca14f258386d3e75e94c853c23841e96b7
Igor Nikolaev <igor.sv.n@gmail.com>
2019-07-19 09:02:15 +0300

    Add "single_instance" option.

--- Metadata/Configuration/Configuration.php
+++ Metadata/Configuration/Configuration.php
@@ -44,6 +44,7 @@ class Configuration implements ConfigurationInterface
                 ->booleanNode('oauth_only')->defaultFalse()->end()
                 ->arrayNode('order_by')->prototype('enum')->values(['asc', 'desc'])->end()->end()
                 ->arrayNode('searchable_fields')->prototype('scalar')->end()->end()
+                ->booleanNode('single_instance')->defaultFalse()->end()
                 ->arrayNode('sortable_fields')->prototype('scalar')->end()->end()
                 ->arrayNode('route_blacklist')->prototype('scalar')->end()->defaultValue([
                     AdminRouterInterface::TYPE_COPY,

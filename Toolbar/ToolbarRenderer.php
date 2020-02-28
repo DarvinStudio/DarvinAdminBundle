5c42f781ee585b584e505c2647df95d46804dde0
Igor Nikolaev <igor.sv.n@gmail.com>
2020-02-28 16:15:36 +0300

    Add dummy toolbar renderer.

---
+++ Toolbar/ToolbarRenderer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2020, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Darvin\AdminBundle\Toolbar;
+
+/**
+ * Toolbar renderer
+ */
+class ToolbarRenderer implements ToolbarRendererInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function renderToolbar(): ?string
+    {
+        return null;
+    }
+}

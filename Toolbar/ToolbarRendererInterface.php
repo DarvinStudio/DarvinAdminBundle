23bccbae263af3699e8cbd4ed11933bb288c2aec
Igor Nikolaev <igor.sv.n@gmail.com>
2020-02-28 16:14:38 +0300

    Add toolbar renderer interface.

---
+++ Toolbar/ToolbarRendererInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2020, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Darvin\AdminBundle\Toolbar;
+
+/**
+ * Toolbar renderer
+ */
+interface ToolbarRendererInterface
+{
+    /**
+     * @return string|null
+     */
+    public function renderToolbar(): ?string;
+}

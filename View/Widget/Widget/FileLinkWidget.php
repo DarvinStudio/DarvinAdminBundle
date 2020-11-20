23cbb393a5dfad588d5a49ce93b64b668e745b5c
Igor Nikolaev <igor.sv.n@gmail.com>
2020-11-20 13:39:21 +0300

    Add "file_link" view widget.

--- View/Widget/Widget/FileLinkWidget.php
+++ View/Widget/Widget/FileLinkWidget.php
@@ -57,6 +57,6 @@ class FileLinkWidget extends AbstractWidget
             return null;
         }
 
-        return sprintf('<a href="%s" target="_blank">%1$s</a>', $url);
+        return sprintf('<a href="%1$s" target="_blank">%1$s</a>', $url);
     }
 }

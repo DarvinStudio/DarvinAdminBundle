7471acb6e2d88684da501798b1856e0e4d18cd1e
Igor Nikolaev <igor.sv.n@gmail.com>
2020-11-20 11:38:27 +0300

    Add dummy file link view widget.

---
+++ View/Widget/Widget/FileLinkWidget.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2020, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\AdminBundle\View\Widget\Widget;
+
+/**
+ * File link view widget
+ */
+class FileLinkWidget extends AbstractWidget
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function createContent(object $entity, array $options): ?string
+    {
+        return __METHOD__;
+    }
+}
